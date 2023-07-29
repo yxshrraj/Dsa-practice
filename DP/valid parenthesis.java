f330e463d94193da57b26a9f1b3ba5d824365634
Yash Raj <86347034+yxshrraj@users.noreply.github.com>
2023-07-29 18:41:26 +0530

    Create valid parenthesis.java

---
+++ DP/valid parenthesis.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String s) 
+    {
+        Stack <Character> st =new Stack<>();
+        for(int i=0 ;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            if( ch =='(' || ch =='['|| ch =='{' )
+            {
+                st.push(ch);
+            }
+            else 
+            {
+               if(st.isEmpty()){
+                   return false;
+               }
+               char x =st.peek();
+               if(ch==')' && x=='(' || ch==']' && x=='[' || ch=='}' && x== '{')
+               {
+                   st.pop();
+               }
+               else
+                 return false;
+            }
+        }
+        if(st.isEmpty())
+          return true;
+
+          return false;
+    }
+}
+
