ed6c75ccbe73eef693d7b12bb2f9e5d23b6f75e4
Yash Raj <86347034+yxshrraj@users.noreply.github.com>
2023-08-05 19:09:00 +0530

    singlenumber.java
    brute n optimal both

--- August/singlenumber.java
+++ August/singlenumber.java
@@ -20,3 +20,15 @@ return single;
 
     }
 }
+
+optimal :
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int i=0 ;i<nums.length; i++)
+         {
+             xor =xor ^ nums[i];
+         }
+         return xor;
+    }
+}
