7603cf0ed098e6072c56a2821199d46625513998
Yash Raj <86347034+yxshrraj@users.noreply.github.com>
2023-08-12 15:08:24 +0530

    Update 2sum.java

--- August/2sum.java
+++ August/2sum.java
@@ -1,4 +1,28 @@
+brute force:
 class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n= nums.length;
+        int [] ans =new int[2];
+        for(int i= 0 ; i<n ; i++)
+        {
+            for(int j=i+1 ; j<n;j++)
+            {
+                int sum = nums[i]+nums[j];
+                if(sum==target)
+                {
+                    ans[0]=i;
+                    ans[1]=j;
+                }
+            }
+        }
+        return ans;
+    }
+}
+---------------------------------------------------------------------------------
+    
+    optimal:
+
+    class Solution {
     public int[] twoSum(int[] nums, int target) {
         int ans[] =new int[2];
         Arrays.sort(nums);
