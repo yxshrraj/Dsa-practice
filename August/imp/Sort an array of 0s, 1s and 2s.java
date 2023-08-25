ae77232782e87b69345fe76c7f275e00b79383ee
Yash Raj <86347034+yxshrraj@users.noreply.github.com>
2023-08-25 23:24:26 +0530

    Update Sort an array of 0s, 1s and 2s.java

--- August/imp/Sort an array of 0s, 1s and 2s.java
+++ August/imp/Sort an array of 0s, 1s and 2s.java
@@ -27,3 +27,35 @@ class Solution
     }
     
 }
+------------------------------------------------------------------------------------------------------------------------------------------
+    Optimal :
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        // code here
+        int mid=0 ;
+        int low=0;
+        int high= n-1;
+        while(mid<=high)
+        {
+            if(a[mid]==0)
+            {
+                int temp = a[mid];
+                a[mid]=a[low];
+                a[low]=temp;
+                low++;
+                mid++;
+            }
+            else if(a[mid]==1) mid++;
+            
+            else
+            {
+                int temp = a[mid];
+                a[mid]=a[high];
+                a[high]=temp;
+                high--;
+            }
+        }
+    }
+}
