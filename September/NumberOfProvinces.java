63f7b3702accc48d5b0f587344d6ccc51211bc56
Yash Raj <86347034+yxshrraj@users.noreply.github.com>
2023-09-13 12:33:31 +0530

    NumberOfProvinces.java
    leet + GFG

--- September/NumberOfProvinces.java
+++ September/NumberOfProvinces.java
@@ -44,4 +44,49 @@ class Solution {
     }
 }
 ____________________________________________________________________________________________________________________________________________________________________________
-GFG wla :
+GFG wla
+    
+class Solution {
+    static void dfs(int node, ArrayList<ArrayList<Integer>> list,boolean vis[])
+    {
+        vis[node]=true;
+        for(Integer it : list.get(node))
+        {
+            if(vis[it]==false)
+            {
+                vis[it]=true;
+                dfs(it,list,vis);
+            }
+        }
+    }
+    static int numProvinces(ArrayList<ArrayList<Integer>> adj, int V) {
+        // code here
+        ArrayList<ArrayList<Integer>> list= new ArrayList<ArrayList<Integer>>();
+        for(int i=0 ;i<V;i++)
+        {
+            list.add(new ArrayList<Integer>());
+        }
+        for(int i=0; i<V; i++)
+        {
+            for(int j=0; j<V;j++)
+            {
+                if(adj.get(i).get(j)==1 && i!=j)
+                {
+                    list.get(i).add(j);
+                    list.get(j).add(i);
+                }
+            }
+        }
+        boolean vis[]= new boolean [V];
+        int cnt=0;
+        for(int i=0 ;i<V;i++)
+        {
+            if(vis[i]==false)
+            {
+                cnt++;
+                dfs(i,list,vis);
+            }
+        }
+        return cnt;
+    }
+};
