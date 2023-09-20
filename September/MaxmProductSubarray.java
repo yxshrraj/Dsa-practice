51a7df68be1a131c104e4fcaf7047cd4da0856f1
Yash Raj <86347034+yxshrraj@users.noreply.github.com>
2023-09-20 23:54:29 +0530

    MaxmProductSubarray.java

--- September/MaxmProductSubarray.java
+++ September/MaxmProductSubarray.java
@@ -19,3 +19,23 @@ class Solution {
     }
 }
 
+Better:
+class Solution {
+    public int maxProduct(int[] nums) {
+        int pref=1;
+        int suff=1;
+        int ans=Integer.MIN_VALUE;
+        int n=nums.length;
+        for(int i=0; i<n ;i++)
+        {
+            if(pref==0) pref=1;
+            if(suff==0) suff=1;
+
+            pref*=nums[i];
+            suff*=nums[n-i-1];
+              ans= Math.max(ans,Math.max(suff,pref));
+
+        }
+        return ans;
+    }
+}
