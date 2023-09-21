4ba1302f0dee22a1a523ef8c883b05b5205fa7d9
Yash Raj <86347034+yxshrraj@users.noreply.github.com>
2023-09-21 11:45:33 +0530

    01Knapsack.java

--- 01Knapsack.java
+++ 01Knapsack.java
@@ -37,3 +37,30 @@ class Solution
        return ks(wt,val,n-1,W,dp);
     } 
 }
+Tabulation :
+static int knapSack(int W, int wt[], int val[], int n) 
+    { 
+         // your code here 
+         int dp[][]= new int[n][W+1];
+        
+         for(int i= wt[0] ; i<=W ;i++)
+         {
+             dp[0][i]=val[0];
+         }
+         for(int ind=1; ind<n ;ind++)
+         {
+             for(int w=0; w<=W; w++)
+             {
+                 int notake= 0+ dp[ind-1][w];
+                 int take= Integer.MIN_VALUE;
+                 if(wt[ind]<=w)
+                 {
+                     take= val[ind]+ dp[ind-1][w-wt[ind]];
+                 }
+                 dp[ind][w]=Math.max(take,notake);
+             }
+         }
+         return dp[n-1][W];
+         
+    } 
+}
